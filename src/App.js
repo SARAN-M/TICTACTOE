5fa025e1d76dcb66babc88eb784865cabb13d845
SARAN-M <saranmyilsamy@gmail.com>
2020-11-21 10:39:31 +0530

    Icons are imported from Components and layout for the game is ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
+import React,{useState} from "react"
+import Icons from "./components/icon"
+import {ToastContainer,toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import {Card, CardBody,Container,Button,Col,Row} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.css"
 import './App.css';
-
+const arr=new Array(9).fill("empty");
 function App() {
+  const [iscross,setIscross]= useState(false)
+  const [winmsg,setWinmsg]=useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container className='p-5'>
+    <ToastContainer position="bottom-center"/>
+    <Row>
+    <Col md={6} className="offset-md-3">
+    <div className="grid">  
+    {arr.map((item,id)=>(
+      <Card>
+        <CardBody className='box'>
+       <Icons name={item}/>
+        </CardBody>
+      </Card>
+
+    ))}
+        </div>  
+    </Col>
+    </Row>
+  </Container>
   );
 }
 
