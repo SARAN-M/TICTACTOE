3225a9bd6935d85b70ce804434bdb80d86ea618e
SARAN-M <saranmyilsamy@gmail.com>
2020-11-21 10:48:36 +0530

    choosing the winner logic is implemented

--- src/App.js
+++ src/App.js
@@ -15,42 +15,60 @@ function App() {
     setWinmsg("")
     arr.fill("empty",0,9)
   }
-  const change=itemno=>{
-    if(winmsg)
-      return toast(winmsg,{type:"success"})
-
-    if(arr[itemno] === "empty"){
-      arr[itemno]= iscross ? "cross" : "circle"
-      setIscross(!iscross)
-    }
-    else{
-      return toast("already filled",{type:"error"})
-    }
-}
-  return (
-    <Container className='p-5'>
-      <ToastContainer position="bottom-center"/>
-      <Row>
-      <Col md={6} className="offset-md-3">
-        {winmsg ? (<div>
-        <h1>{winmsg}</h1>
-          <Button onClick={reload}>reload the game</Button>
-          </div>) : (<h1>{iscross ? "cross" : "circle"} turns</h1>)}
-      <div className="grid">  
-      {arr.map((item,id)=>(
-        <Card onClick={()=>change(id)}>
-          <CardBody className='box'>
-         <Icons name={item}/>
-          </CardBody>
-        </Card>
-  
-      ))}
-          </div>  
-      </Col>
-      </Row>
-    </Container>
-    );
+  const checkwin=()=>{
+    if(arr[0]==arr[1] && arr[0]==arr[2] && arr[0]!="empty")
+      setWinmsg(`${arr[0]} wins`)
+    if(arr[3]==arr[4] && arr[3]==arr[5] && arr[3]!="empty")
+      setWinmsg(`${arr[3]} wins`)
+    if(arr[6]==arr[7] && arr[6]==arr[8] && arr[6]!="empty")
+      setWinmsg(`${arr[6]} wins`)
+    if(arr[0]==arr[3] && arr[0]==arr[6] && arr[0]!="empty")
+      setWinmsg(`${arr[0]} wins`)
+    if(arr[1]==arr[4] && arr[1]==arr[7] && arr[1]!="empty")
+      setWinmsg(`${arr[1]} wins`)
+    if(arr[2]==arr[5] && arr[2]==arr[8] && arr[2]!="empty")
+      setWinmsg(`${arr[2]} wins`)
+    if(arr[0]==arr[4] && arr[0]==arr[8] && arr[0]!="empty")
+      setWinmsg(`${arr[0]} wins`)
+    if(arr[2]==arr[4] && arr[2]==arr[6] && arr[2]!="empty")
+      setWinmsg(`${arr[2]} wins`)
   }
-  
-  export default App;
-  
+  const change=itemno=>{
+        if(winmsg)
+          return toast(winmsg,{type:"success"})
+
+        if(arr[itemno] === "empty"){
+          arr[itemno]= iscross ? "cross" : "circle"
+          setIscross(!iscross)
+        }
+        else{
+          return toast("already filled",{type:"error"})
+        }
+        checkwin();
+  }
+  return (
+  <Container className='p-5'>
+    <ToastContainer position="bottom-center"/>
+    <Row>
+    <Col md={6} className="offset-md-3">
+      {winmsg ? (<div>
+      <h1>{winmsg}</h1>
+        <Button onClick={reload}>reload the game</Button>
+        </div>) : (<h1>{iscross ? "cross" : "circle"} turns</h1>)}
+    <div className="grid">  
+    {arr.map((item,id)=>(
+      <Card onClick={()=>change(id)}>
+        <CardBody className='box'>
+       <Icons name={item}/>
+        </CardBody>
+      </Card>
+
+    ))}
+        </div>  
+    </Col>
+    </Row>
+  </Container>
+  );
+}
+
+export default App;
