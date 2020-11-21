b099393d0be41ae53be06014efee4095312806e1
SARAN-M <saranmyilsamy@gmail.com>
2020-11-21 10:46:06 +0530

    changing the item and reloading the game are implemented

--- src/App.js
+++ src/App.js
@@ -10,25 +10,47 @@ const arr=new Array(9).fill("empty");
 function App() {
   const [iscross,setIscross]= useState(false)
   const [winmsg,setWinmsg]=useState("")
-  return (
-  <Container className='p-5'>
-    <ToastContainer position="bottom-center"/>
-    <Row>
-    <Col md={6} className="offset-md-3">
-    <div className="grid">  
-    {arr.map((item,id)=>(
-      <Card>
-        <CardBody className='box'>
-       <Icons name={item}/>
-        </CardBody>
-      </Card>
+  const reload=()=>{
+    setIscross(false)
+    setWinmsg("")
+    arr.fill("empty",0,9)
+  }
+  const change=itemno=>{
+    if(winmsg)
+      return toast(winmsg,{type:"success"})
 
-    ))}
-        </div>  
-    </Col>
-    </Row>
-  </Container>
-  );
+    if(arr[itemno] === "empty"){
+      arr[itemno]= iscross ? "cross" : "circle"
+      setIscross(!iscross)
+    }
+    else{
+      return toast("already filled",{type:"error"})
+    }
 }
-
-export default App;
+  return (
+    <Container className='p-5'>
+      <ToastContainer position="bottom-center"/>
+      <Row>
+      <Col md={6} className="offset-md-3">
+        {winmsg ? (<div>
+        <h1>{winmsg}</h1>
+          <Button onClick={reload}>reload the game</Button>
+          </div>) : (<h1>{iscross ? "cross" : "circle"} turns</h1>)}
+      <div className="grid">  
+      {arr.map((item,id)=>(
+        <Card onClick={()=>change(id)}>
+          <CardBody className='box'>
+         <Icons name={item}/>
+          </CardBody>
+        </Card>
+  
+      ))}
+          </div>  
+      </Col>
+      </Row>
+    </Container>
+    );
+  }
+  
+  export default App;
+  
